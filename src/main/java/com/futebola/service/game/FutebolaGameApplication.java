c983e5a7a72ffaa3c573d8edf7c0675d828d9c01
Cesar Granelli <cesar.granelli@gmail.com>
2024-12-07 16:28:57 -0300

    first commit

---
+++ src/main/java/com/futebola/service/game/FutebolaGameApplication.java
@@ -0,0 +1,13 @@
+package com.futebola.service.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutebolaGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutebolaGameApplication.class, args);
+    }
+
+}
